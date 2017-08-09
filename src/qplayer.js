b7e41d0ca5c5257e419dcdad453ef71151d11b4d
alsotang <alsotang@gmail.com>
2017-08-09 18:18:14 +0800

    方法定义

--- src/qplayer.js
+++ src/qplayer.js
@@ -1,8 +1,58 @@
-function QPlayer() {
+
+
+function QPlayer(options) {
+  var self = this;
+
+  self.__width = null;
+  self.__height = null;
+  // 是否直播
+  self.__live = null;
+  // 是否自动播放
+  self.__autoplay = null;
+
+  // 视频类型
+  self.__vType = null;
+  //
+  self.__source = null;
+
+  self.__volume = null;
+
+  self.setOptions(options)
+}
+
+QPlayer.prototype.setOptions = function (options) {
 
 }
 
-QPlayer.prototype.init = function () {
+QPlayer.prototype.play = function (options) {
+
+}
+
+QPlayer.prototype.pause = function (options) {
+
+}
+
+QPlayer.prototype.mute = function (options) {
+
+}
+
+QPlayer.prototype.setVolume = function (options) {
+
+}
+
+QPlayer.prototype.isPlaying = function (options) {
+
+}
+
+QPlayer.prototype.getDuration = function () {
+
+}
+
+QPlayer.prototype.getCurrentTime = function () {
+
+}
+
+QPlayer.prototype.fullScreen = function () {
 
 }
 
